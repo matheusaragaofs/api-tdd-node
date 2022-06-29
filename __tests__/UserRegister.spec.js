ce7035c288f07b26e9c82edd2f8fe7b6ac343370
Matheus <matheusaragaofs@gmail.com>
2022-06-28 21:47:20 -0300

    dynamic tests

--- __tests__/UserRegister.spec.js
+++ __tests__/UserRegister.spec.js
@@ -15,14 +15,12 @@ const validUser = {
   username: 'user1',
   email: 'user@email.com',
   password: 'P4ssword',
-}
+};
 const postUser = (user = validUser) => {
   return request(app).post('/api/1.0/users').send(user);
 };
 
 describe('User registration', () => {
-
-
   it('returns 200 OK when signup request is valid', async () => {
     const response = await postUser();
     expect(response.status).toBe(200);
@@ -59,42 +57,41 @@ describe('User registration', () => {
       email: 'user@email.com',
       password: 'P4ssword',
     });
-    expect(response.status).toBe(400)
-  })
+    expect(response.status).toBe(400);
+  });
   it('returns validationErrors field in response body when validation errors occurs', async () => {
     const response = await postUser({
       username: null,
       email: 'user@email.com',
       password: 'P4ssword',
     });
-    const body = response.body
-    expect(body.validationErrors).not.toBeUndefined()
-  })
-  it('returns Username can not be null when username is null errors occurs', async () => {
-    const response = await postUser({
-      username: null,
-      email: 'user@email.com',
-      password: 'P4ssword',
-    });
-    const body = response.body
-    expect(body.validationErrors.username).toBe("Username can not be null")
-  })
-  it('returns E-mail can not be null when username is null errors occurs', async () => {
-    const response = await postUser({
-      username: 'user1',
-      email: null,
-      password: 'P4ssword',
-    });
-    const body = response.body
-    expect(body.validationErrors.email).toBe("E-mail can not be null")
-  })
+    const body = response.body;
+    expect(body.validationErrors).not.toBeUndefined();
+  });
+
   it('returns errors for both  when username and email is null', async () => {
     const response = await postUser({
       username: null,
       email: null,
       password: 'P4ssword',
     });
-    const body = response.body
-    expect(Object.keys(body.validationErrors)).toEqual['username', 'email']
-  })
+    const body = response.body;
+    expect(Object.keys(body.validationErrors)).toEqual[('username', 'email')];
+  });
+
+  it.each([
+    ['username', 'Username can not be null'],
+    ['email', 'E-mail can not be null'],
+    ['password', 'Password cannot be null'],
+  ])('when %s is null %s is recieved', async (field, expectedMessage) => {
+    const user = {
+      username: 'user1',
+      email: 'user@email.com',
+      password: 'P4ssword',
+    };
+    user[field] = null;
+    const response = await postUser(user);
+    const body = response.body;
+    expect(body.validationErrors[field]).toBe(expectedMessage);
+  });
 });
--- src/user/UserRouter.js
+++ src/user/UserRouter.js
@@ -1,24 +1,26 @@
 const express = require('express');
 
 const UserService = require('./UserService');
-const router = express.Router()
-const { check, validationResult } = require('express-validator')
+const router = express.Router();
+const { check, validationResult } = require('express-validator');
 
-router.post('/api/1.0/users',
+router.post(
+  '/api/1.0/users',
   check('username').notEmpty().withMessage('Username can not be null'),
   check('email').notEmpty().withMessage('E-mail can not be null'),
+  check('password').notEmpty().withMessage('Password cannot be null'),
   async (req, res) => {
-    const errors = validationResult(req)
+    const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      const validationErrors = {
-      }
-      errors.array().forEach(error => {
-        return validationErrors[error.param] = error.msg
-      })
-      return res.status(400).send({ validationErrors })
+      const validationErrors = {};
+      errors.array().forEach((error) => {
+        return (validationErrors[error.param] = error.msg);
+      });
+      return res.status(400).send({ validationErrors });
     }
     await UserService.save(req.body);
     return res.send({ message: 'User created' });
-  });
+  }
+);
 
 module.exports = router;
