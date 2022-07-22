be55dd3d4a5fd6cc9bca922044e17bc7ff71cdc7
Matheus <matheusaragaofs@gmail.com>
2022-07-21 21:29:18 -0300

    Authentication

---
+++ __tests__/Auth.spec.js
@@ -0,0 +1,39 @@
+const request = require('supertest');
+const app = require('../src/app');
+const User = require('../src/user/User');
+const sequelize = require('../src/config/database');
+const bcrypt = require('bcrypt')
+beforeAll(async () => {
+    await sequelize.sync()
+})
+beforeEach(async () => {
+    await User.destroy({ truncate: true })
+})
+
+const addUser = async () => {
+    const user = { username: 'user1', email: 'user1@email.com', password: 'P4ssword', incative: false }
+    const hash = await bcrypt.hash(user.password, 10)
+    user.password = hash;
+    return await User.create(user)
+}
+const postAuthentication = async (credentials) => {
+    return await request(app).post('/api/1.0/auth').send(credentials)
+}
+
+describe('Authentication', () => {
+    it('returns 200 when credentials are correct', async () => {
+        await addUser()
+        const response = await postAuthentication({ email: 'user1@email.com', password: 'P4ssword' })
+        expect(response.status).toBe(200)
+    });
+    it('returns only user id and username when login success', async () => {
+        const user = await addUser()
+        const response = await postAuthentication({ email: 'user1@email.com', password: 'P4ssword' })
+        expect(response.body.id).toBe(user.id)
+        expect(response.body.username).toBe(user.username)
+        expect(Object.keys(response.body)).toEqual(['id', 'username'])
+
+    });
+
+
+}); 1
--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const UserRouter = require('./user/UserRouter');
 const ErrorHandler = require('./error/ErrorHandler')
+const AuthenticationRouter = require('./auth/AuthenticationRouter')
 const app = express();
 app.use(express.json());
 
 app.use(UserRouter);
+app.use(AuthenticationRouter)
 app.use(ErrorHandler)
 
 module.exports = app;
