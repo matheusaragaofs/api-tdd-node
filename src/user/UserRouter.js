f7d9a6b92cd2ae66c77feb9ec9361665cd291561
matheus_aragao <matheusaragaofs@gmail.com>
2022-12-29 15:13:48 -0300

    Username Validation

--- src/user/UserRouter.js
+++ src/user/UserRouter.js
@@ -85,16 +85,33 @@ router.get('/api/1.0/users/:id', async (req, res, next) => {
 
 })
 
-router.put('/api/1.0/users/:id', async (req, res, next) => {
-  const authenticatedUser = req.authenticatedUser
+router.put('/api/1.0/users/:id',
+  check('username')
+    .notEmpty()
+    .withMessage('Username cannot be null')
+    .bail()
+    .isLength({ min: 4, max: 32 })
+    .withMessage('Must have min 4 and max 32 characters'),
+  async (req, res, next) => {
 
-  if (!authenticatedUser || authenticatedUser.id != req.params.id) {
-    return next(new ForbiddenExecption('You are not authorized to update user'))
-  }
-  const updatedUser = await UserService.updateUser(req.params.id, req.body)
-  return res.send(updatedUser)
 
-})
+    const authenticatedUser = req.authenticatedUser
+
+
+    if (!authenticatedUser || authenticatedUser.id != req.params.id) {
+      return next(new ForbiddenExecption('You are not authorized to update user'))
+    }
+
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return next(new ValidationException(errors.array()))
+    }
+
+    const updatedUser = await UserService.updateUser(req.params.id, req.body)
+    return res.send(updatedUser)
+
+  })
 
 router.delete('/api/1.0/users/:id', async (req, res, next) => {
   const authenticatedUser = req.authenticatedUser
