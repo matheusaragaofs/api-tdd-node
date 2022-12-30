116afa4239f82863b9a4d0685e7965969c35c899
matheus_aragao <matheusaragaofs@gmail.com>
2022-12-30 17:59:34 -0300

    Refactoring Configuration

---
+++ database/config.js
@@ -0,0 +1,11 @@
+const profiles = require('../config')
+
+const dbConfigs = {}
+
+Object.keys(profiles).forEach((profile) => {
+  dbConfigs[profile] = {
+    ...profiles[profile].database
+  }
+})
+
+module.exports = dbConfigs
