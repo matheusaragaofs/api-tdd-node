d4b2dd6eacddd16730a02fd27f5a496d31506878
matheus_aragao <matheusaragaofs@gmail.com>
2022-12-30 10:51:41 -0300

    Migration Demo

---
+++ database/seeders/20221230133712-add-users.js
@@ -0,0 +1,33 @@
+'use strict';
+const bcrypt = require('bcrypt')
+
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    const hash = await bcrypt.hash('P4ssword', 10)
+    const users = []
+    for (let i = 0; i < 25; i++) {
+      users.push({
+        username: `user${i + 1}`,
+        email: `user${i + 1}@email.com`,
+        inactive: false,
+        password: hash,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      })
+    }
+
+    await queryInterface.bulkInsert('users', users, {})
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('users', null, {})
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
