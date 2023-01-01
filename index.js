c2d740928b617b3aaeff48be6cdb09412175b2d8
Matheus Aragao <matheusaragaofs@gmail.com>
2023-01-01 10:14:57 -0300

    Loggin App Version

--- index.js
+++ index.js
@@ -3,14 +3,6 @@ const sequelize = require('./src/config/database');
 const TokenService = require('./src/auth/TokenService');
 const logger = require('./src/shared/logger');
 sequelize.sync();
-
-logger.error('error 1');
-logger.warn('warn 2');
-logger.info('info 3');
-logger.verbose('verbose 4');
-logger.debug('debug 5');
-logger.silly('silly 6');
-
 TokenService.scheduledCleanup();
 
-app.listen(3000, () => logger.info('app is running!'));
+app.listen(3000, () => logger.info('app is running! Version is ' + process.env.npm_package_version));
